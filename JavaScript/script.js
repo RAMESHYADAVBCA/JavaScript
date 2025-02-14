2cab74ab91bff621d6dbaf7874c51a9499286070
RAMESHYADAVBCA <ry275886@gmail.com>
2025-02-14 08:37:32 +0530

    Add code

---
+++ JavaScript/script.js
@@ -0,0 +1,46 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("Ramesh");
+console.log(23);
+
+let firstName = "Ram";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Ramesh";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+
+//console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
